8ef5b93e0d57b7a28094894ac12f0da319658649
Joshua M Hertlein <jmhertlein@gmail.com>
2014-07-02 22:33:07 -0500

    Wrote a real readme
    Added license and COPYING.txt
    Added GPL blurb on program execution
    Added gitattributes file

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-NSA irc surveillance
+# PyFoxBot
 
-NSA loves furries
+## Summary
+
+An IRC bot for #Foxhole on irc.rizon.net.
+
+## License and Copying
+
+Licensed under the GNU GPLv3+, see COPYING.txt for more details.
+
+## Contributing
+
+All code must be GPLv3+. Work on your own fork and open a pull request.
+
+One work item (feature, bugfix, etc) per PR.
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
+"""
+Copyright (C) 2014 TheFoxHole (https://github.com/TheFoxHole)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
 """Main module for PyFoxBot"""
 
+def print_intro():
+    print("PyFoxBot Copyright (C) 2014  TheFoxHole (https://github.com/TheFoxHole)")
+    print("This program comes with ABSOLUTELY NO WARRANTY.")
+    print("This is free software, and you are welcome to redistribute it")
+    print("under certain conditions; see COPYING.txt for more details..")
 
 def main():
-    pass
-
+    print_intro()
 
 if __name__ == "__main__":
     main()
+
